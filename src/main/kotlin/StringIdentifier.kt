8daa32adf29a3ba7a13872a6e71b5a2ab6ece4c5
GiuliaBortone <giulia.bortone@mail.polimi.it>
2024-03-18 12:29:12 +0100

    change implementation of StringCategoriser, each identifying function can be called

---
+++ src/main/kotlin/StringIdentifier.kt
@@ -0,0 +1,14 @@
+class StringIdentifier {
+    fun isFlowing(word: String): Boolean {
+        val sortedWord = word.toCharArray().sorted().joinToString("")
+        return word == sortedWord
+    }
+
+    fun isReceding(word: String): Boolean {
+        return isFlowing(word.reversed())
+    }
+
+    fun isTurbulent (word: String): Boolean {
+        return !isFlowing(word) || !isReceding(word)
+    }
+}
---
+++ src/test/kotlin/StringIdentifierTest.kt
@@ -0,0 +1,43 @@
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.Test
+
+class StringIdentifierTest {
+    private val categoriser = StringIdentifier()
+
+    @Test
+    fun `one letter strings are both flowing and receding`() {
+        assertThat(categoriser.isFlowing("a")).isTrue()
+        assertThat(categoriser.isReceding("a")).isTrue()
+    }
+
+    @Test
+    fun `two same letters strings are both flowing and receding`() {
+        assertThat(categoriser.isFlowing("aa")).isTrue()
+        assertThat(categoriser.isReceding("aa")).isTrue()
+    }
+
+    @Test
+    fun `two letters string dn is flowing`() {
+        assertThat(categoriser.isFlowing("dn")).isTrue()
+    }
+
+    @Test
+    fun `two letters string zj is receding`() {
+        assertThat(categoriser.isReceding("zj")).isTrue()
+    }
+
+    @Test
+    fun `three letters string abz is flowing`() {
+        assertThat(categoriser.isFlowing("abz")).isTrue()
+    }
+
+    @Test
+    fun `three letters string xhc is receding`() {
+        assertThat(categoriser.isReceding("xhc")).isTrue()
+    }
+
+    @Test
+    fun `three letters word ghg is turbulent`() {
+        assertThat(categoriser.isTurbulent("ghg")).isTrue()
+    }
+}
